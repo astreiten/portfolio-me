d20591ab0c698993c032a335ca9170c5b433cf1c
Agustin Streitenberger <a_streiten@outlook.com>
2022-05-27 18:26:15 -0300

    * convert into microsite

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import * as ReactDOMClient from "react-dom/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let root: ReactDOMClient.Root;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//@ts-ignore
+window.renderMe = (containerId, history) => {
+  const container = document.getElementById(containerId);
+  //@ts-ignore
+  root = ReactDOMClient.createRoot(container);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+//@ts-ignore
+window.unmountMe = () => {
+  //@ts-ignore
+  root.unmount();
+};
